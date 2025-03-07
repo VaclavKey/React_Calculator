4aa3e1dfd7c49ddd63707ee07f8f468989e5ffe6
iris <scout20010@gmail.com>
2025-03-07 15:51:59 +0300

    Add Calculator component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Calculator from './components/Calculator'
 import './App.css'
 
 function App() {
-  
+  return (
+  <Calculator />
+  );
 }
 
 export default App
